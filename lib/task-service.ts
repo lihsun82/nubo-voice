32c6fe6fae4f54e67d5a039b9378495319377d7a
lihsun82 <84670777+lihsun82@users.noreply.github.com>
2026-06-21 19:03:22 +0800

    Remove unused task service scaffold

--- lib/task-service.ts
+++
@@ -1,7 +0,0 @@
-import { getTask } from "@/lib/task-store";
-
-export async function runTask(id: string) {
-  const task = await getTask(id);
-  if (!task) throw new Error("找不到指定任務");
-  return task;
-}
