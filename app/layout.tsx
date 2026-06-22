de7f4698565045bdb708b64a04a531812661a957
lihsun82 <84670777+lihsun82@users.noreply.github.com>
2026-06-23 01:18:26 +0800

    Load NUBO technology interface styles

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import "./globals.css";
 import "./task-center.css";
 import "./integration-center.css";
 import "./youtube-player.css";
+import "./nubo-tech.css";
 
 export const metadata: Metadata = {
   title: "NUBO Voice",
