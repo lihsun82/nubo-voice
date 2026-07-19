61a950cbab80492887d5b6467eead4c59b8f9d94
lihsun82 <84670777+lihsun82@users.noreply.github.com>
2026-07-19 21:27:39 +0800

    載入小智語音核心樣式

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,7 @@ import "./integration-center.css";
 import "./youtube-player.css";
 import "./orb-theme.css";
 import "./mobile-pwa.css";
+import "./xiaozhi-provider.css";
 
 export const metadata: Metadata = {
   title: {
