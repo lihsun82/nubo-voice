764a623b7517de542fd9ba64bf844f484727fc0a
lihsun82 <84670777+lihsun82@users.noreply.github.com>
2026-08-02 13:16:50 +0800

    style: load inline music player styles

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,6 +9,7 @@ import "./youtube-player.css";
 import "./orb-theme.css";
 import "./mobile-pwa.css";
 import "./mobile-privacy-v11.css";
+import "./inline-music-v13.css";
 
 export const metadata: Metadata = {
   title: {
